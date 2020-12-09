4e52fc6c87061ecd86aa3496075e80c8decceaa1
toneDef00 <daniels@dual4t.com>
2020-12-08 19:31:12 -0800

    start of basic firebase function for use with azure

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,24 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const cors = require('cors');
+const sql = require('mssql')
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+app.get('/api/:deviceId', (req, res) => res.send(Widgets.getById(req.params.id)));
+app.post('/', (req, res) => res.send(Widgets.create()));
+app.put('/:id', (req, res) => res.send(Widgets.update(req.params.id, req.body)));
+app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
+app.get('/', (req, res) => res.send(Widgets.list()));
+
+// Expose Express API as a single Cloud Function:
+exports.widgets = functions.https.onRequest(app);
